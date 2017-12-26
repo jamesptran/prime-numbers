2071df911fd9d6f3ab3ef5a9d1d337c0dff9b47d
Phuc Tran <jamesptran96@gmail.com>
2017-12-26 14:56:25 -0500

    init

---
+++ Prime_Numbers.h
@@ -0,0 +1,19 @@
+
+#ifndef PARALLEL_PRIME_NUMBERS_H
+#define PARALLEL_PRIME_NUMBERS_H
+
+# include <cstdlib>
+# include <limits.h>
+# include <iostream>
+# include <cstring>
+# include <omp.h>
+# include <math.h>
+
+using namespace std;
+
+class Prime_Numbers {
+
+};
+
+
+#endif //PARALLEL_PRIME_NUMBERS_H
